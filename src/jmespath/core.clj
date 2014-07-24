62ce1bb88240b769535f0b92aa67009568fbc3c3
James Saryerwinnie <js@jamesls.com>
2014-07-23 20:01:22 -0700

    Start of jmespath grammar
    Completely unambiguous subset.

--- src/jmespath/core.clj
+++ src/jmespath/core.clj
@@ -6,51 +6,18 @@
 
 (def ^:private parser
   (insta/parser
-    "<exp>                 = stat | pipe-expr
-     pipe-expr             = exp <'|'> exp
-     <stat>                = group | projection | literal | condition |
-                             index | chunk | sub-expr | current-node
-     <chunk>               = identifier | function-expr |
-                             multi-select-list | multi-select-hash
-     sub-expr              = (exp <'.'> chunk) | (exp index)
-     index                 = <'['> number <']'>
-     number                = '-'* digit+
-     <digit>               = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' |
-                             '8' | '9'
-     <group>               = <'('> exp <')'>
+    "<expression>          = sub-expression | identifier | index-expression | multi-select-list | multi-select-hash
      identifier            = unquoted-string | quoted-string
-     <unquoted-string>     = #'[A-Za-z]+[0-9A-Za-z_]*'
-     <quoted-string>       = <'\"'> #'(?:\\|\\\"|[^\"])*' <'\"'>
-     literal               = <'`'> #'(?:\\|\\`|[^`])*' <'`'>
-     <projection>          = value-projection |
-                             flatten-projection |
-                             index-projection |
-                             filter-projection
-     projection-subject    = identity | exp
-     projection-predicate  = identity | stat | <'.'> stat
-     identity              = EPSILON
-     value-projection      = projection-subject [<'.'>] <'*'> projection-predicate
-     index-projection      = projection-subject <'[*]'> projection-predicate
-     flatten-projection    = projection-subject <'[]'> projection-predicate
-     filter-projection     = projection-subject <'[?'> condition <']'> projection-predicate
-     <condition>           = unary-condition | binary-condition
-     binary-condition      = stat binop stat
-     unary-condition       = negation | stat
-     negation              = <'!'> stat
-     binop                 = '<' | '<=' | '>' | '>=' | '==' | '!=' | '&&' | '||'
-     function-expr         = function-name <'('> function-args <')'>
-     function-name         = unquoted-string
-     function-args         = [explist]
-     <explist>             = explist-item [{<','> explist-item}]
-     <explist-item>        = exp | expr-type
-     current-node          = <'@'>
-     expr-type             = <'&'> exp
-     multi-select-list     = <'['> explist <']'>
-     multi-select-hash     = <'{'> keyval-list <'}'>
-     <keyval-list>         = keyval-exp [{<','> keyval-exp}]
-     keyval-exp            = keyval-key <':'> keyval-value
-     keyval-key            = identifier
-     keyval-value          = exp"
+     multi-select-list     = <'['> expression (<','> expression)* <']'>
+     multi-select-hash     = <'{'> keyval-expr (<','> keyval-expr)* <'}'>
+     keyval-expr           = identifier <':'> expression
+     unquoted-string       = #'[A-Za-z]+[0-9A-Za-z_]*'
+     quoted-string         = <'\"'> #'(?:\\|\\\"|[^\"])*' <'\"'>
+     index-expression      = expression bracket-specifier | bracket-specifier
+     bracket-specifier     = <'['> number <']'>
+     number                = '-'* digit+
+     <digit>                 = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+     sub-expression        = expression <'.'> (identifier | multi-select-list | multi-select-hash)"
     :auto-whitespace (insta/parser "whitespace = #'\\s+'")))
 
 (defn parse [exp]
@@ -62,3 +29,6 @@
 (defn search [exp data]
   "Searches the provides data structures using a JMESPath expression"
   (interpret (parse exp) data))
+
+(defn check [exp]
+  (insta/parses parser exp))
